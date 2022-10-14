a5382d2b1d8778636e0cfc44981d710558008beb
Purity Shihafu <puritieshee99@gmail.com>
2022-10-14 09:16:02 -0700

    my commits

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>                                                                                  
+int main(void) {
+
+	/* puts() displays the string inside quotation */
+	
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
